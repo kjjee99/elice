6a4f6bf4511d6a5377b535806e45568571aa839f
jungeun <kjjee99@naver.com>
2022-08-04 22:16:14 +0900

    Initial commit

---
+++ src/test/java/com/kit/my/elice/EliceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kit.my.elice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
